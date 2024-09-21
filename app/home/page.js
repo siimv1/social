23354321da2a0f020be45d96826c723f3376561d
svainrau <siim.vainrauh@gmail.com>
2024-09-21 17:08:36 +0300

    post fix

--- app/home/page.js
+++ app/home/page.js
@@ -174,7 +174,6 @@ const Home = () => {
 
                 <div className="timeline-section">
                     <h2>Your Timeline</h2>
-                    {/* Removed the hardcoded "John Doe" */}
                     <PostList newPost={newPost} />  {/* Dynamic post list */}
                 </div>
             </div>
